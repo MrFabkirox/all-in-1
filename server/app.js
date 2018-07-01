7f7933d299eca11f3fb1ab45cca99a48ee8e5c8d
Fabrice Esope <fabrice@fairfx.com>
2018-07-01 06:52:03 +0100

    graphQL http link

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
-const express = require('express')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
-const app = express()
+const app = express();
+
+app.use('/graphql',graphqlHTTP({
+
+}));
+
 
 app.listen(4000, () => {
 
---
+++ server/schema/schema.js
@@ -0,0 +1,15 @@
+
+const graphql = require('graphql');
+
+const {GraphQLObjectType} = graphql;
+
+const BookType = new GraphQLObjectType({
+
+  name:'Book'.
+  
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+  )}
+
+)};
