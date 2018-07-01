a00fbc9a43d3ed0741cb6050be9f7d308f5a0038
Fabrice Esope <fabrice@fairfx.com>
2018-07-01 07:33:10 +0100

    schema module half way through ?

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,15 +1,36 @@
 
 const graphql = require('graphql');
 
-const {GraphQLObjectType} = graphql;
+
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
 const BookType = new GraphQLObjectType({
 
-  name:'Book'.
+  name:'Book',
   
   fields: () => ({
     id: {type: GraphQLString},
     name: {type: GraphQLString},
+    genre: {type: GraphQLString}
   )}
 
 )};
+
+const RootQuery = new GraphQLObjectType({
+  name:'RootQuery',
+  fields:{
+    book:{
+      type:BookType,
+      args:{id:{type:GraphQLString}}
+      resolve(parent.args){
+       // code to get data from db or other source
+      }
+    }
+  }
+)};
+
+export.module.GraphQLSchema({
+  query:RootQuery
+
+
+)}
